a14aaffe674bacc79a8b6d46760a2b0f3208eb47
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-13 01:52:55 +0800

    Code Rewrite v1.2

---
+++ Main.py
@@ -0,0 +1,23 @@
+class Utility:
+	class Out:
+		def lnStr(curStr): print(curStr)
+		def lnInt(curNum): print(curNum)
+
+LocalUtil = Utility()
+
+class Math:
+	class Operations:
+		def add(a, b): print(a+b)
+		def subtract(a, b): print(a-b)
+		def multiply(a, b): print(a*b)
+		def divide (a, b): print(a/b)
+
+ExtMath = Math()
+
+class Main:
+
+	def run():
+
+		# Write Code
+		
+Main.run()
