e7234a7c260eb6c1b13ccadf4c9d675d174f4d38
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-01-13 02:01:55 +0800

    Rewriting code again.

--- Main.py
+++ Main.py
@@ -1,23 +1,34 @@
+# V-Script (PY)
+# Release Tag: 1.2
+# Release Name: "Code Rewrite"
+
 class Utility:
-	class Out:
-		def lnStr(curStr): print(curStr)
-		def lnInt(curNum): print(curNum)
+    class Out:
+        def lnStr(curStr): print(curStr)
+        def lnInt(curNum): print(curNum)
+
+    class Version:
+        def Check():
+            curName = "V-Script"
+            curTag ="1.2"
+            tagName = "Code Rewrite"
+            print("Name: " + curName + "\nVersion Tag: v" + curTag + "\nTag Name: " + tagName)
 
 LocalUtil = Utility()
 
 class Math:
-	class Operations:
-		def add(a, b): print(a+b)
-		def subtract(a, b): print(a-b)
-		def multiply(a, b): print(a*b)
-		def divide (a, b): print(a/b)
+    class Operations:
+        def add(a, b): print(a+b)
+        def subtract(a, b): print(a-b)
+        def multiply(a, b): print(a*b)
+        def divide (a, b): print(a/b)
 
 ExtMath = Math()
 
 class Main:
 
-	def run():
+    def run():
 
-		# Write Code
-		
+        # Write Code
+        
 Main.run()
